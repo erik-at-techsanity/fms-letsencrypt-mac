6b33003471f65d3e9d862dbdea4bad8c8154116a
Erik Williamson <erik@techsanity.ca>
2022-12-03 15:46:27 -0500

    WIP

--- install_service.sh
+++ install_service.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
-PREFIX=$(dirname ${BASH_SOURCE})
+PREFIX=$(dirname "${BASH_SOURCE[0]}")
 
-source ${PREFIX}/common.sh
+source "${PREFIX}/common.sh"
+if [[ ! -e "${PREFIX}/config.sh" ]] ; then
+  echo "ERROR: Please ensure config.sh exists. You may need to copy it from config.dist.sh and edit it."
+  exit ${E_NO_CONFIG}
+fi
+source "${PREFIX}/config.sh"
 
-log "INFO" "Copying ${PLIST_FILE} to ${LAUNCH_DAEMONS_DIR}"
-cp ${PLIST_FILE} ${LAUNCH_DAEMONS_DIR}
+function egress {
+  if [[ -e ${TEMP_FILE} ]] ; then
+    log "INFO" "Removing ${TEMP_FILE}"
+    rm -f ${TEMP_FILE}
+  fi
+}
+
+trap egress EXIT
+
+# Checks to see if script is running as root.
+if [[ ${EUID} -ne 0 ]] ; then
+  echo "Please run as root."
+  exit ${E_USER}
+fi
+
+TEMP_FILE=$(mktemp)
+log "INFO" "Copying ${PLIST_TEMPLATE_FILE} to ${TEMP_FILE}"
+cp "${PREFIX}/${PLIST_TEMPLATE_FILE}" "${TEMP_FILE}"
+
+### Sed time
+sed -i BAK "s|%%INSTALL_DIR%%|${INSTALL_DIR}|" "${TEMP_FILE}"
+sed -i BAK "s|%%SERVICE_NAME%%|${SERVICE_NAME}|" "${TEMP_FILE}"
+
+log "INFO" "Copying ${TEMP_FILE} to ${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}"
+cp "${TEMP_FILE}" "${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}"
 
 log "INFO" "Changing ownership of ${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}"
-chown root:wheel ${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}
+chown root:wheel "${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}"
 
 log "INFO" "Changing file mode of ${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}"
-chmod o-w ${LAUNCH_DAEMONS_DIR}/${PLIST_FILE} 
+chmod o-w "${LAUNCH_DAEMONS_DIR}/${PLIST_FILE}"
 
 log "INFO" "Done"
