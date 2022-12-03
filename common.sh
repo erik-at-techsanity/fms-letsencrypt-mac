2ae62bca7f86edfe39b68f82d739f61f3845b3f2
Erik Williamson <erik@techsanity.ca>
2022-12-03 15:45:29 -0500

    initial commit

---
+++ common.sh
@@ -0,0 +1,25 @@
+#
+# Return Codes
+#
+E_OK=0
+E_USAGE=1
+E_USER=2
+E_MISSING_REQUIREMENTS=3
+E_FMS_SERVER_NOT_RUNNING=4
+E_NO_CONFIG=5
+
+#
+# Functions
+#
+function log {
+  LEVEL=$1
+  MESSAGE=$2
+  TIMESTAMP=$(date +"%Y-%m-%d %H-%I-%S")
+
+  echo "${TIMESTAMP} : ${LEVEL} : ${MESSAGE}"
+}
+
+function fms_process_count {
+  COUNT=$(pgrep "fmserverd|fmsib|fmsased|fmslogtrimmer|fmserver_helperd" | wc -l | awk '{ print $1 }')
+  echo "${COUNT}"
+}
